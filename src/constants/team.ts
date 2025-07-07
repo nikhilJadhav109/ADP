5204336af446e8b61ac148919dc4eced58cdda04
swapnilshitole72 <swapnilshitole72@gmail.com>
2025-07-07 20:45:32 +0530

    Added experience in tema

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -1,28 +1,28 @@
   export const TEAM = [
   {
     name: 'Komal Dhanuka',
-    experience : "25+ years",
+    experience : "25+ years Experience",
     position: 'Founder and Director',
     image: 'team/komal.jpg',
     bio: 'Leads the vision and design excellence at Agrawal Ply Decor.'
   },
   {
     name: 'Kaustubh Dhanuka',
-    experience : "2+ years",
+    experience : "2+ years Experience",
     position: 'Managing Director',
     image: 'team/kaustabh.jpg',
     bio: 'Drives operations and ensures strategic growth of the company.'
   },
   {
     name: 'Manish Singh',
-    experience : "10+ years",
+    experience : "10+ years Experience",
     position: 'Business Development Manager',
     image: 'team/manish.jpg',
     bio: 'Builds client relations and drives business expansion.'
   },
   {
     name: 'Arvind Vishwakarma',
-    experience : "25+ years",
+    experience : "25+ years Experience",
     position: 'Production Head',
     image: 'team/Arvind.jpeg',
     bio: 'Manages the production process, ensuring efficiency, quality, and timely delivery.'
