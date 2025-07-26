680362107cb02908325bb8edc02c430224a28e4a
Nikhil Anil Jadhav <njadhav@koorier.ca>
2025-07-26 17:20:46 +0530

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')!).render(
+ReactGA.initialize("G-NK6CVJM0NK");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
